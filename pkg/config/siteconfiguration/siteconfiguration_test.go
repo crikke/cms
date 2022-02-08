0596af424257e630b9a143d4f3c5a6033e12b885
crikke <crikke95@gmail.com>
2022-02-08 20:04:34 +0100

    Test for Siteconfiguration MessageHandler

--- pkg/config/siteconfiguration/siteconfiguration_test.go
+++ pkg/config/siteconfiguration/siteconfiguration_test.go
@@ -1,1 +1,41 @@
 package siteconfiguration
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/streadway/amqp"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/text/language"
+)
+
+func Test_MessageHandlerUpdatesConfiguration(t *testing.T) {
+
+	ch := make(chan amqp.Delivery)
+	done := make(chan error)
+	cfg := &Configuration{
+		Languages: []language.Tag{
+			language.Swahili,
+		},
+	}
+
+	newCfg := &Configuration{
+		Languages: []language.Tag{
+			language.Norwegian,
+		},
+	}
+
+	data, err := json.Marshal(newCfg)
+
+	assert.NoError(t, err)
+
+	go messageHandler(cfg, ch, done)
+	ch <- amqp.Delivery{
+		Body: data,
+	}
+	close(ch)
+
+	err = <-done
+	assert.NoError(t, err)
+	assert.Equal(t, language.Norwegian, cfg.Languages[0])
+}
