47bed0e5ca19515015aeeb7903c0bee46fbf6fa8
crikke <crikke95@gmail.com>
2022-02-22 22:23:36 +0100

    rename

--- pkg/contentmanagement/propertydefinition/validator/validator.go
+++ pkg/contentmanagement/propertydefinition/validator/validator.go
@@ -20,8 +20,8 @@ import (
 */
 // Validator runs when content is Saved
 
-type RequiredField bool
-type RegexField string
+type RequiredRule bool
+type RegexRule string
 
 type Validator interface {
 	Validate(ctx context.Context, field interface{}) error
@@ -32,12 +32,12 @@ func Parse(name string, val interface{}) (Validator, error) {
 	switch name {
 	case "required":
 		if b, ok := val.(bool); ok {
-			return RequiredField(b), nil
+			return RequiredRule(b), nil
 		}
 		return nil, errors.New("parse error: cannot parse into type RequiredField")
 	case "pattern":
 		if str, ok := val.(string); ok {
-			return RegexField(str), nil
+			return RegexRule(str), nil
 		}
 	}
 
@@ -46,14 +46,14 @@ func Parse(name string, val interface{}) (Validator, error) {
 
 // Validators
 
-func (r RequiredField) Validate(ctx context.Context, field interface{}) error {
+func (r RequiredRule) Validate(ctx context.Context, field interface{}) error {
 	if bool(r) && field == nil {
 		return errors.New("required")
 	}
 	return nil
 }
 
-func (r RegexField) Validate(ctx context.Context, field interface{}) error {
+func (r RegexRule) Validate(ctx context.Context, field interface{}) error {
 
 	str := string(r)
 	b, ok := field.([]byte)
